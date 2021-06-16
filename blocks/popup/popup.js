f269aa0729bafb1db4443da35749cc20c70e2323
Igor Volnov <ivolnov@a-bt.ru>
2021-06-16 18:26:46 +0300

    fix

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -10,55 +10,31 @@ const subtitleInput = popup.querySelector('.popup__input_type_subtitle');
 
 const titleText = document.querySelector('.profile__title');
 const subtitleText = document.querySelector('.profile__subtitle');
-const body = document.querySelector('body');
-
-function checkSaveButtonStatus() {
-    if (titleInput.value.length > 0 && subtitleInput.value.length > 0) {
-        saveButton.removeAttribute('disabled');
-    } else {
-        saveButton.setAttribute('disabled', 'disabled');
-    }
-}
 
 function keyAction(e) {
-    if (e.keyCode === 27) closePopup();
     if (e.keyCode === 13) saveData();
 }
-
-
 
 function openPopup() {
     titleInput.value = titleText.textContent;
     subtitleInput.value = subtitleText.textContent;
     popup.classList.add('popup_visible');
-    body.style.overflow = 'hidden';
     document.addEventListener('keydown', keyAction);
-    popup.addEventListener('click', closeOnClickAway, false);
-    checkSaveButtonStatus();
 }
 
 function closePopup() {
     popup.classList.remove('popup_visible');
-    body.style.overflow = 'auto';
     document.removeEventListener('keydown', keyAction);
-    popup.removeEventListener('click', closeOnClickAway, false);
-}
-
-function closeOnClickAway(e) {
-    if (e.target.classList.contains('popup_visible')) {
-        closePopup();
-    }
 }
 
 function saveData(e) {
-    if (e) e.preventDefault();
+    e.preventDefault();
     titleText.textContent = titleInput.value;
     subtitleText.textContent = subtitleInput.value;
     closePopup();
 }
 
-titleInput.addEventListener('input', checkSaveButtonStatus);
-subtitleInput.addEventListener('input', checkSaveButtonStatus);
+
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', saveData);
